5fcfdaf3cf2037eab366b9b7df3fe01e6975295b
Yeeun Park <anna862700@gmail.com>
2023-02-24 22:16:36 +0900

    feat: echo -n 옵션 기능 수정

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: yeepark <yeepark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 15:32:44 by yeepark           #+#    #+#             */
-/*   Updated: 2023/02/16 21:06:57 by yeepark          ###   ########.fr       */
+/*   Updated: 2023/02/24 22:15:39 by yeepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,37 @@
 
 extern t_global	g_global;
 
+int	is_option(char *str)
+{
+	int	idx;
+
+	idx = 0;
+	if (str[idx] != '-')
+		return (0);
+	idx++;
+	while (str[idx] && str[idx] == 'n')
+		idx++;
+	return (!str[idx]);
+}
+
 void	ft_echo(t_node *node)
 {
 	int	idx;
+	int	option;
 	int	newline;
 
 	idx = 1;
+	option = 1;
 	newline = 1;
-	if (!ft_strcmp(node->command_arg[1], "-n"))
-	{
-		newline = 0;
-		idx++;
-	}
 	while (node->command_arg[idx])
 	{
+		if (option && is_option(node->command_arg[idx]))
+		{
+			newline = 0;
+			idx++;
+			continue ;
+		}
+		option = 0;
 		printf("%s", node->command_arg[idx]);
 		idx++;
 		if (node->command_arg[idx])
