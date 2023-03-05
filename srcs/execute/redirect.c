ca8cd337e9a56f0e0bb7326d09bc1310fefc2a37
Yeeun Park <anna862700@gmail.com>
2023-03-05 20:21:12 +0900

    feat: INPUT REDIRECT 파일이 존재하지 않는 경우 예외처리

--- srcs/execute/redirect.c
+++ srcs/execute/redirect.c
@@ -6,7 +6,7 @@
 /*   By: yeepark <yeepark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 16:58:38 by yeepark           #+#    #+#             */
-/*   Updated: 2023/02/25 19:58:09 by yeepark          ###   ########.fr       */
+/*   Updated: 2023/03/05 20:18:47 by yeepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,22 +23,38 @@ int	is_out(t_redirect_type type)
 		|| type == APPENDING_REDIRECTED_OUTPUT);
 }
 
-// in open => access 로 체크 
-void	handle_redirect(t_node *node)
+void	handle_in_redirect(t_node *node)
+{
+	if (node->redirect_type == REDIRECTING_INPUT)
+	{
+		if (access(node->redirect_filename, F_OK | R_OK) == -1)
+		{
+			ft_putstr_fd("bash: ", 2);
+			ft_putstr_fd(node->redirect_filename, 2);
+			ft_putstr_fd(": command not found\n", 2);
+			exit(1);
+		}
+		node->in_fd = open(node->redirect_filename, O_RDWR);
+	}
+	dup2(node->in_fd, STDIN_FILENO);
+}
+
+void	handle_out_redirect(t_node *node)
 {
 	int	mode;
 
-	if (node->redirect_type == REDIRECTING_INPUT)
-		node->in_fd = open(node->redirect_filename, O_RDWR);
-	if (is_in(node->redirect_type))
-		dup2(node->in_fd, STDIN_FILENO);
 	if (node->redirect_type == REDIRECTING_OUTPUT)
 		mode = O_RDWR | O_CREAT | O_TRUNC;
 	if (node->redirect_type == APPENDING_REDIRECTED_OUTPUT)
 		mode = O_RDWR | O_CREAT | O_APPEND;
+	node->out_fd = open(node->redirect_filename, mode, 0644);
+	dup2(node->out_fd, STDOUT_FILENO);
+}
+
+void	handle_redirect(t_node *node)
+{
+	if (is_in(node->redirect_type))
+		handle_in_redirect(node);
 	if (is_out(node->redirect_type))
-	{
-		node->out_fd = open(node->redirect_filename, mode, 0644);
-		dup2(node->out_fd, STDOUT_FILENO);
-	}
+		handle_out_redirect(node);
 }
