b9bb3c8ebf498421c86a9513b66351d0309580d9
Yeeun Park <anna862700@gmail.com>
2023-03-10 20:33:23 +0900

    feat: OLDPWD 없는 경우 환경변수 리스트에 추가

--- srcs/envp.c
+++ srcs/envp.c
@@ -6,7 +6,7 @@
 /*   By: yeepark <yeepark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 16:14:45 by yeepark           #+#    #+#             */
-/*   Updated: 2023/02/16 17:19:56 by yeepark          ###   ########.fr       */
+/*   Updated: 2023/03/10 20:30:55 by yeepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,26 @@ extern t_global	g_global;
 int	init_envp(char *envp[])
 {
 	int				idx;
+	int				is_oldpwd;
 	t_env			*new;
 
 	idx = 0;
+	is_oldpwd = 0;
 	while (envp[idx])
 	{
 		new = make_env(envp[idx]);
 		if (g_global.errno != NaE)
 			return (1);
 		add_env_back(new);
+		if (!strcmp(new->name, "OLDPWD"))
+			is_oldpwd = 1;
 		idx++;
 	}
+	if (is_oldpwd)
+		return (0);
+	new = make_env("OLDPWD");
+	if (g_global.errno != NaE)
+		return (1);
+	add_env_back(new);
 	return (0);
 }
