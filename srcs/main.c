824fda4c0bae932f203bc4896148735a9fbff095
Yeeun Park <anna862700@gmail.com>
2023-03-26 22:15:01 +0900

    refactor: apply norminette

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,29 @@
 /*   By: rjeong <rjeong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 16:57:24 by rjeong            #+#    #+#             */
-/*   Updated: 2023/03/26 20:24:00 by yeepark          ###   ########.fr       */
+/*   Updated: 2023/03/26 21:53:20 by yeepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 t_global	g_global;
+
+void	set_main(t_parsed *parsed)
+{
+	*parsed = (t_parsed){NULL, NULL, NULL};
+	g_global.err_num = NaE;
+	init_standard_fildes();
+	handle_signal();
+	unset_comtrol_character();
+}
+
+void	free_all(t_parsed *parsed)
+{
+	free_token_list(&parsed->token_list);
+	free_token_list(&parsed->compressed_list);
+	free_tree(parsed->tree);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -24,37 +40,22 @@ int	main(int argc, char **argv, char **envp)
 	init_global(envp);
 	while (1)
 	{
-		parsed = (t_parsed) {NULL, NULL, NULL};
-		g_global.err_num = NaE;
-		init_standard_fildes();
-		handle_signal();
-		unset_comtrol_character();
+		set_main(&parsed);
 		input = readline("\033[34mminishell-1.0$ \033[0m");
 		if (input == NULL)
 		{
 			printf("exit\n");
 			exit(g_global.exit_status);
 		}
-		// parsing
-
 		parse(input, &parsed);
-
 		add_history(input);
 		free(input);
-		input = 0;
-
 		if (!parsed.tree)
-			continue;
-
-		// executing
+			continue ;
 		search_heredoc(parsed.tree);
 		search_tree(parsed.tree);
-
-		free_token_list(&parsed.token_list);
-		free_token_list(&parsed.compressed_list);
-		free_tree(parsed.tree);
-		
-	//	system("leaks --list minishell");
+		free_all(&parsed);
+		system("leaks --list minishell");
 	}
 	return (0);
 }
