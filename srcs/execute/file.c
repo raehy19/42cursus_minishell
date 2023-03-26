808347994f0d1f322992ec3f0c1a6eeef74fe0ab
Yeeun Park <anna862700@gmail.com>
2023-03-26 15:58:37 +0900

    feat: file 속성 체크 함수 구현

---
+++ srcs/execute/file.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   file.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeepark <yeepark@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/26 15:32:08 by yeepark           #+#    #+#             */
+/*   Updated: 2023/03/26 15:32:10 by yeepark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+int	is_directory(mode_t st_mode)
+{
+	return ((st_mode & S_IFMT) == S_IFDIR);
+}
+
+int	is_regular_file(mode_t st_mode)
+{
+	return ((st_mode & S_IFMT) == S_IFREG);
+}
+
