617e1320707b9ba465c13589dc7caad1cf5e3b31
Yeeun Park <anna862700@gmail.com>
2023-04-01 16:16:28 +0900

    fix: exit 명령어 문자가 포함된 인자 들어올 때 bash와 똑같이 처리

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: yeepark <yeepark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 11:03:19 by yeepark           #+#    #+#             */
-/*   Updated: 2023/03/30 16:49:53 by yeepark          ###   ########.fr       */
+/*   Updated: 2023/04/01 16:14:46 by yeepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,17 @@ void	ft_exit(t_node *node)
 		ft_putstr_fd("exit\n", 2);
 	if (node->arg_cnt == 1)
 		exit(g_global.exit_status);
-	g_global.exit_status = 1;
-	if (node->arg_cnt > 2)
-	{
-		print_command_error(node, 0, "too many arguments");
-		return ;
-	}
 	if (!ft_isnumber(node->command_arg[1]))
 	{
+		g_global.exit_status = 255;
 		print_command_error(node, 1, "numberic argument required");
 		exit(g_global.exit_status);
+	}
+	if (node->arg_cnt > 2)
+	{
+		g_global.exit_status = 1;
+		print_command_error(node, 0, "too many arguments");
+		return ;
 	}
 	g_global.exit_status = ft_atoi(node->command_arg[1]);
 	exit(g_global.exit_status);
