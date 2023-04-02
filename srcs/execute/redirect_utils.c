5cb6ff1455ec10da1c49fba91a692826382c876b
Yeeun Park <anna862700@gmail.com>
2023-04-02 12:45:50 +0900

    fix: input redirect 존재하지 않는 파일 들어올때 에러 메시지 출력 수정

--- srcs/execute/redirect_utils.c
+++ srcs/execute/redirect_utils.c
@@ -6,7 +6,7 @@
 /*   By: yeepark <yeepark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 16:43:24 by yeepark           #+#    #+#             */
-/*   Updated: 2023/04/01 17:05:05 by yeepark          ###   ########.fr       */
+/*   Updated: 2023/04/02 12:45:10 by yeepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,15 @@ int	check_in_redirect_authority(char *filename)
 	char	*error_message;
 
 	error_message = 0;
-	if (access(filename, R_OK) == -1)
-	{
-		error_message = "Permission denied\n";
-		g_global.err_num = PERMISSION_DENIED;
-	}
-	if (!error_message && access(filename, F_OK | R_OK) == -1)
+	if (access(filename, F_OK) == -1)
 	{
 		error_message = "No such file or directory\n";
 		g_global.err_num = NO_SUCH_FILE;
+	}
+	if (!error_message && access(filename, R_OK) == -1)
+	{
+		error_message = "Permission denied\n";
+		g_global.err_num = PERMISSION_DENIED;
 	}
 	if (error_message)
 		print_redirect_error(filename, error_message);
